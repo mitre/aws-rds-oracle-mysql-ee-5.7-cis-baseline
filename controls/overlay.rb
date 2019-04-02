b6a0b16e21f28f4279aaaa28c6d528df24ce52f8
Sturtevant, Alicia A. (ASTURTEVANT) <a.sturtevant4@gmail.com>
2019-04-02 11:50:44 -0400

    updates to convert this profile into an overlay

---
+++ controls/overlay.rb
@@ -0,0 +1,200 @@
+include_controls 'Oracle-MySQL-Enterprise-Edition-5.7-cis-baseline' do
+
+	control "1.1" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "1.2" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "1.3" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+
+	control "1.4" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "2.1" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "2.2" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "3.1" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "3.2" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "3.3" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "3.4" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "3.5" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "3.6" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "3.7" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "3.8" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "3.9" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "4.3" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "4.5" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "6.10" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "6.11" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "6.2" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "6.4" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "6.5" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "6.6" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "6.7" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "6.8" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "6.9" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "7.3" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+	control "9.1" do
+	  impact 0.0
+	  describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+	    skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+	  end
+	end
+
+end
