ce6494b717e8bbdbe9b601ca85621c072ed0d529
ejaronne <34140975+ejaronne@users.noreply.github.com>
2021-01-12 22:02:26 -0500

    Rds adjustments (#9)
    * restate sensitive inputs
    
    to maintain sensitive requirements at the baseline level
    
    * remove defunct mysql_slave_users input
    
    * add mysql_users
    
    * Update overlay.rb
    
    * Update overlay.rb
    
    * Update overlay.rb
    
    * Set 9.2 to N/A since rds does not support this setting
    
    * switching to master dependency as merging from bottom up
    
    * version alignment to base 1.0.8 prep for merge

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -124,6 +124,13 @@ include_controls 'oracle-mysql-ee-5.7-cis-baseline' do
       skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
     end
   end
+  
+  control '4.6' do
+    impact 0.0
+    describe 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on' do
+      skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
+    end
+  end
 
   control '6.10' do
     impact 0.0
@@ -187,6 +194,27 @@ include_controls 'oracle-mysql-ee-5.7-cis-baseline' do
       skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
     end
   end
+  
+  control '6.12' do
+    tag "check": "(AWS RDS note: AWS supports this type of plugin via the MariaDB SERVER_AUDIT plugin:
+    https://aws.amazon.com/premiumsupport/knowledge-center/advanced-audit-rds-mysql-cloudwatch/)
+    To assess this recommendation, execute the following SQL statement:
+      SELECT LOAD_OPTION FROM information_schema.plugins WHERE PLUGIN_NAME='SERVER_AUDIT';
+    The result must be FORCE_PLUS_PERMANENT"
+    tag "fix": "To remediate this setting, follow these steps, configure the settings of your 
+     MariaDB Audit Plugin on your AWS custom option group"
+  
+    query = %{SELECT LOAD_OPTION FROM information_schema.plugins WHERE PLUGIN_NAME='SERVER_AUDIT';}
+    sql_session = mysql_session(attribute('user'), attribute('password'), attribute('host'), attribute('port'))
+
+    audit_log_plugin = sql_session.query(query).stdout.strip
+
+    describe 'The MySQL audit plugin (SERVER_AUDIT)' do
+      subject { audit_log_plugin }
+      it { should cmp 'FORCE_PLUS_PERMANENT' }
+    end
+
+  end
 
   control '7.3' do
     impact 0.0
@@ -195,6 +223,24 @@ include_controls 'oracle-mysql-ee-5.7-cis-baseline' do
     end
   end
   
+  control '7.5' do
+    tag "check": "Execute the following SQL query to determine if any users have a blank password:
+        SELECT user,host 
+        FROM mysql.user 
+        WHERE (plugin IN('mysql_native_password', 'mysql_old_password') 
+          AND LENGTH(password) = 0 AND LENGTH(authentication_string) = 0);
+    No rows will be returned if all accounts have a password set."
+
+    query = %{SELECT user,host FROM mysql.user WHERE (plugin IN('mysql_native_password', 'mysql_old_password') AND LENGTH(password) = 0 AND LENGTH(authentication_string) = 0);}
+    sql_session = mysql_session(attribute('user'), attribute('password'), attribute('host'), attribute('port'))
+    users_with_blank_passwords = sql_session.query(query).stdout.strip
+
+    describe 'The MySQL users with blank passwords' do
+      subject { users_with_blank_passwords }
+      it { should be_empty }
+    end
+  end
+
   control '7.6' do
     
     validate_password_plugin_status_query = %{SELECT plugin_status from information_schema.plugins where plugin_name like 'validate_password%'}
@@ -272,4 +318,12 @@ include_controls 'oracle-mysql-ee-5.7-cis-baseline' do
       skip 'This control is not applicable on mysql within aws rds, as aws manages the operating system in which the mysql database is running on'
     end
   end
+  
+  control '9.2' do
+    impact 0.0
+    describe 'This control is not applicable on mysql within aws rds, as aws rds does not support this setting' do
+      skip 'This control is not applicable on mysql within aws rds, as aws rds does not support this setting'
+    end
+  end
+
 end
